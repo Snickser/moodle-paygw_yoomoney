fb05c0827968ec9dece5cf712001e8aa395e2c72
root <snickser@gmail.com>
2024-07-26 11:52:56 +0300

    add

--- callback.php
+++ callback.php
@@ -38,6 +38,8 @@ $invid     = required_param('label', PARAM_TEXT); // TEXT only!
 $amount    = required_param('amount', PARAM_TEXT); // TEXT only!
 $signature = required_param('sha1_hash', PARAM_ALPHANUMEXT);
 
+$unaccepted = required_param('unaccepted', PARAM_TEXT);
+
 $nt   = required_param('notification_type', PARAM_TEXT);
 $opid = required_param('operation_id', PARAM_TEXT);
 $dt   = required_param('datetime', PARAM_TEXT);
@@ -45,6 +47,10 @@ $sdr  = required_param('sender', PARAM_TEXT);
 
 if (empty($invid)) {
     throw new Error('FAIL. Empty transaction id.');
+}
+
+if ($unaccepted == 'true') {
+    throw new Error('FAIL. Unaccepted payment.');
 }
 
 if (!$yoomoneytx = $DB->get_record('paygw_yoomoney', ['paymentid' => $invid])) {
@@ -75,6 +81,32 @@ $crc = hash('sha1', "$nt&$opid&$amount&643&$dt&$sdr&false&$secret&$invid");
 
 if ($signature !== $crc) {
     throw new Error('FAIL. Signature does not match.');
+}
+
+$data = "operation_id=$opid";
+$location = 'https://yoomoney.ru/api/operation-details';
+$options = [
+    'CURLOPT_RETURNTRANSFER' => true,
+    'CURLOPT_TIMEOUT' => 30,
+    'CURLOPT_HTTP_VERSION' => CURL_HTTP_VERSION_1_1,
+    'CURLOPT_SSLVERSION' => CURL_SSLVERSION_TLSv1_2,
+    'CURLOPT_HTTPHEADER' => [
+        'Authorization: Bearer ' . $config->token,
+        'Content-Type: application/x-www-form-urlencoded',
+    ],
+    'CURLOPT_FOLLOWLOCATION' => false,
+];
+$curl = new curl();
+$jsonresponse = $curl->post($location, $data, $options);
+$response = json_decode($jsonresponse);
+
+if ($config->savedebugdata) {
+    file_put_contents($CFG->dataroot . '/payment.log', date("Y-m-d H:i:s") . "\n" .
+    serialize($response) . "\n\n", FILE_APPEND | LOCK_EX);
+}
+
+if ($response->status !== "success") {
+    throw new Error('FAIL. Payment status error.');
 }
 
 // Update payment.
