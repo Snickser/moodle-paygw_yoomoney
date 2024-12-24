66561b5488e8eaab21accd48914ba331def7c6d3
Snickser <snickser@gmail.com>
2024-12-24 18:46:22 +0300

    version

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2024122400;
+$plugin->version   = 2024122401;
 $plugin->requires  = 2023100900;
 $plugin->component = 'paygw_yoomoney';
-$plugin->release   = '0.3';
+$plugin->release   = '0.4';
 $plugin->maturity  = MATURITY_STABLE;
